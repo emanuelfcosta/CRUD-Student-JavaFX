0720196169f3c8f4ea95dba1f627117ef4f26e43
Emanuel Costa <emanuel.costa.dev@gmail.com>
2023-10-18 23:28:00 -0300

    Create Database.sql

---
+++ Database.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `student` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) DEFAULT NULL,
+  `address` varchar(150) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
